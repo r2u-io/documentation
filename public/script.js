5e732da542a19e950253ce2366f4f6ef993f4590
Luan <luan.dev.santos@gmail.com>
2020-05-19 17:33:15 -0300

    updating lib and implementing new features

--- public/script.js
+++ public/script.js
@@ -3,6 +3,8 @@ const customerId = '5e8e7580404328000882f4ae'
 const sku = 'RE000001'
 const name = 'Cadeira Eames'
 const popup = false
+const progressBarPosition = 'middle' // top | middle | bottom
+const poster = 'cadeira.png'
 
 Real2U.init({customerId})
   .then(() => {
@@ -10,7 +12,7 @@ Real2U.init({customerId})
       console.log(`[Real2U] sku ${sku} ativo? ${isActive}`)
       if(isActive) {
         const element = document.getElementById('3d-viewer')
-        Real2U.create3DViewer({element, sku, name, popup})
+        Real2U.create3DViewer({element, sku, name, popup, progressBarPosition, poster})
 
         const arButton = document.getElementById('ar-button')
         arButton.onclick = () => Real2U.openAR({sku, name})
