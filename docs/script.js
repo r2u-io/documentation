a47c67a31ca06cfab107ba2f8ae85f98d1e3db55
felipecb <felipe@real2u.com>
2020-09-28 15:35:20 -0300

    Update to js-ar-sdk 3.3.0

--- docs/script.js
+++ docs/script.js
@@ -2,21 +2,19 @@
 const customerId = '5e8e7580404328000882f4ae'
 const sku = 'RE000001'
 const name = 'Cadeira Eames'
-const popup = false
 const progressBarPosition = 'top' // top | middle | bottom
-const progressBarColor='#c5c5c5'
+const progressBarColor = '#c5c5c5'
 const poster = 'https://real2u-public-assets.s3.amazonaws.com/images/cadeira.png'
 
-R2U.init({customerId})
-  .then(() => {
-    R2U.isActive(sku).then(isActive => {
-      console.log(`[R2U] sku ${sku} ativo? ${isActive}`)
-      if(isActive) {
-        const element = document.getElementById('3d-viewer')
-        R2U.create3DViewer({element, sku, name, popup, progressBarPosition, poster, progressBarColor})
+R2U.init({ customerId }).then(() => {
+  R2U.isActive(sku).then((isActive) => {
+    console.log(`[R2U] sku ${sku} ativo? ${isActive}`)
+    if (isActive) {
+      const element = document.getElementById('3d-viewer')
+      R2U.create3DViewer({ element, sku, name, progressBarPosition, poster, progressBarColor })
 
-        const arButton = document.getElementById('ar-button')
-        arButton.onclick = () => R2U.openAR({sku, name})
-      }
-    })
+      const arButton = document.getElementById('ar-button')
+      arButton.onclick = () => R2U.openAR({ sku })
+    }
   })
+})
