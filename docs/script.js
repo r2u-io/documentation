bb767f4ac696a5f06764be28fec28c5c793b5d2a
felipecb <felipe@real2u.com>
2020-09-29 08:41:29 -0300

    Full example documentation

--- docs/script.js
+++ docs/script.js
@@ -6,6 +6,12 @@ const progressBarPosition = 'top' // top | middle | bottom
 const progressBarColor = '#c5c5c5'
 const poster = 'https://real2u-public-assets.s3.amazonaws.com/images/cadeira.png'
 
+const viewer = document.getElementById('viewer-3d')
+const modal = document.getElementById('modal')
+const container = document.getElementById('viewer-container')
+const expandButton = document.getElementsByClassName('expand-button')[0]
+const exitButton = document.getElementsByClassName('exit-button')[0]
+
 R2U.init({ customerId }).then(() => {
   R2U.isActive(sku).then((isActive) => {
     console.log(`[R2U] sku ${sku} ativo? ${isActive}`)
@@ -21,17 +27,49 @@ R2U.init({ customerId }).then(() => {
         progressBarPosition,
         poster,
         progressBarColor
-      }).then(() => {
-
-        const button = document.createElement('button')
-        const img = document.createElement('img')
-        img.src = './ExpandButtonImage.png'
-        img.style = 'width:36px;'
-        button.appendChild(img)
-        button.style = 'bottom:0;right:0;position:fixed;padding:0;border:none;background:none;margin:12px;'
-        element.firstChild.appendChild(button)
-
-      })
+      }).then(initializeElements)
     }
   })
 })
+
+const initializeElements = () => {
+  expandButton.classList.remove('hide')
+
+  expandButton.onclick = onClickExpand
+  exitButton.onclick = onClickExit
+
+  viewer.firstChild.appendChild(expandButton)
+  viewer.firstChild.appendChild(exitButton)
+
+  modal.addEventListener('mousedown', onClickExit)
+  viewer.addEventListener('mousedown', onClickViewer)
+}
+
+const onClickExpand = () => {
+  expandButton.classList.add('hide')
+  exitButton.classList.remove('hide')
+
+  modal.style.display = 'block'
+
+  modal.appendChild(viewer)
+
+  document.getElementsByTagName('model-viewer')[0].classList.add('big')
+}
+
+const onClickExit = () => {
+  expandButton.classList.remove('hide')
+  exitButton.classList.add('hide')
+
+  modal.style.display = 'none'
+
+  container.appendChild(viewer)
+
+  document.getElementsByTagName('model-viewer')[0].classList.remove('big')
+}
+
+const onClickViewer = (e) => {
+  e.preventDefault()
+  e.stopPropagation()
+  e.stopImmediatePropagation()
+  return false
+}
