3ff961d57fdb572f79a2bb92f07bc33824163df5
joseborinr2u <85903121+joseborinr2u@users.noreply.github.com>
2022-08-25 15:40:36 -0300

    Delete only the tabs. (#65)

--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,6 @@ module.exports = {
         'javascript/quickstart',
         'javascript/augmented-reality',
         'javascript/viewer',
-        'javascript/customizer',
         'javascript/analytics',
         'javascript/troubleshooting',
         {
@@ -15,27 +14,6 @@ module.exports = {
           href: 'https://r2u.notion.site/Performance-SDK-af7a42626ba14c1bb264633f8932a222'
         }
       ]
-    },
-    {
-      'React Native SDK': [
-        'react-native/quickstart',
-        'react-native/augmented-reality',
-        'react-native/viewer',
-        'react-native/analytics',
-        'react-native/troubleshooting'
-      ]
-    },
-    {
-      'Flutter SDK': [
-        'flutter/quickstart',
-        'flutter/augmented-reality',
-        'flutter/viewer',
-        'flutter/analytics',
-        'flutter/troubleshooting'
-      ]
-    },
-    {
-      'Swift/Kotlin SDK': ['swift-kotlin/quickstart']
     },
     {
       Guides: [
